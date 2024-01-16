34c3c47bed7d0b8d55ba89e5f4a9fd652d1015ee
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-01-16 15:37:57 +0100

    gjort andara

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // fixa användarnamnet
+        Console.Write("Ange ditt användarnamn: ");
+        string användarnamn = Console.ReadLine();
+        if (användarnamn == "noname")
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
